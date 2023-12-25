b78ba2b2c79e558b6a7d713d393bc3f6e1ac5f83
berkkosal <kosalnberk@gmail.com>
2023-12-25 14:03:05 +0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "./utils/theme.js";
-
-
-
-
-
 
 function App() {
 
