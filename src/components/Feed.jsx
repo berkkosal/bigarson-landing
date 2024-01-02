9f52e421e1164fd2b10cd82f815f364dca9e7fe2
berkkosal <kosalnberk@gmail.com>
2024-01-02 17:06:16 +0300

    updated content

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,21 +1,25 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import ContentCard from './ContentCard'
+import { feedContent } from '../utils/Content'
 
 
-const feedContent = {
-  title1:
-}
+
 
 const Feed = () => {
+
+  const {section1,section2,section3,section4} = feedContent;
+  
+
   return (
     <Box flex={5} bgcolor="pink" >
-      <ContentCard  />
       <ContentCard />
       <ContentCard />
       <ContentCard />
       <ContentCard />
       <ContentCard />
+      <ContentCard />
+      {feedContent.section1.title}
     </Box>
   )
 }
--- src/utils/Content.js
+++ src/utils/Content.js
@@ -1,8 +1,41 @@
-export const feedContent = [
+export const feedContent = {
 
-    {sectionTitle: },
-    {},
-    {},
+    section1: {
+        title: 'QR Menünüzü oluşturun ve her yerde kullanın.',
+        subtitle: 'Restoranınız, websiteniz, Google Maps, Instagram ve Facebook için tek menü',
+        listTitle: 'Dijital menüyü kullanın',
+        listContent: [
+            'Google Maps, Instagram ve Facebooktan müşterileri kendinize çekin.',
+            'Müşterilerinizin restoranda geçirdiği süreyi uzatın',
+            'Basım maliyetlerinden kaçının',
+            'Online menü deneyimini yaşayın'
+        ],
+        imageUrl: ''
+    },
 
+    section2: {
+        title: 'Müşteriler mobil cihazlarda PDF menülerden hoşlanmazlar',
+        imageUrl: ''
+    },
 
-]
+    section3: {
+        title: 'Nasıl Çalışır',
+        iconText1: 'Kayıt ol',
+        iconText2: 'Menü içeriklerinizi oluşturun',
+        iconText3: 'QR Kodunuzu bastırın',
+        buttonText1: 'Menü oluştur',
+        buttonText2: 'Canlı demo alın'
+    },
+
+    section4: {
+        title: 'Özellikler',
+        listTitle: 'Fotoğraflarla Menü',
+        listContent: [
+            'Müşteriler fotoğraflara bayılır',
+            'Mobil cihazlarda harika görünür',
+            'Instagram benzeri kaydırma sağlar'
+        ],
+        imageUrl: ''
+    }
+
+}
