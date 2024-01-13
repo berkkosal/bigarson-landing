6c99bedac7e80368a5bd0d869e92ed659cc7b836
berkkosal <kosalnberk@gmail.com>
2024-01-13 19:56:22 +0300

    last before update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ContentCard from './ContentCard'
 import { feedContent } from '../utils/Content'
 import { Carousel } from './Carousel'
-
+import './Feed.css';
 
 
 
@@ -14,10 +14,13 @@ const Feed = () => {
 
   return (
     <Box flex={5}  >
-
-      <ContentCard content={feedContent.section1} color='#F0F0F0' isEvenly={true} />
-      <Carousel data={feedContent.section1.slides}/>
-      <ContentCard content={feedContent.section2} color='#FFFFFF' />
+      <div className='section1-wrapper'>
+        <ContentCard content={feedContent.section1} color='#F0F0F0' isEvenly={true} />
+      </div>
+      <div className='section1 carousel-wrapper'>
+        <Carousel data={feedContent.section1.slides} />
+      </div>
+      <ContentCard className='section2-wrapper' content={feedContent.section2} color='#FFFFFF' />
       <ContentCard content={feedContent.section3} color='#F0F0F0' />
       <ContentCard content={feedContent.section4} color='#FFFFFF' />
 
